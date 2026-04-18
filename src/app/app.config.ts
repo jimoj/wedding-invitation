68c21fd833f0ece7cf49eaa8c1c83cd1f0c86d00
jaime.jimenez@allianz.com <jaime.jimenez@allianz.com>
2026-04-18 12:50:39 +0200

    feat: Add spaces between elements, avoid hover on mobile, and include toolkit to download photo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { IMAGE_CONFIG } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    { provide: IMAGE_CONFIG, useValue: { disableImageSizeWarning: true, disableImageLazyLoadWarning: true } },
+  ]
 };
